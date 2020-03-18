a33c2bc226df595c6ecc31b0d257fe3880c85c12
Marin Kovač <marinkovac0903@gmail.com>
2020-03-18 14:03:28 +0100

    add some comments for clarity

--- include/sick_safetyscanners/c_api.h
+++ include/sick_safetyscanners/c_api.h
@@ -3,14 +3,19 @@
 typedef struct SickSafetyscannersC SickSafetyscannersC;
 
 typedef struct SickCommunicationParameters {
+//https://cdn.sick.com/media/docs/1/01/701/Technical_information_microScan3_outdoorScan3_en_IM0083701.PDF
 
   std::string sensor_ip_adress;
   std::string host_ip_adress;
   int host_udp_port;
   int channel;
-  double start_angle;
-  double end_angle;
+  double start_angle; //deg
+  double end_angle; //deg
   int skip;
+   // param skip The number of scans to skip between each measured scan.  For a 25Hz laser, setting
+   // 'skip' to 0 makes it publish at 25Hz, 'skip' to 1 makes it publish at 12.5Hz. \return "Publish
+   // Frequency" ie. One out of every n_th scan will be published.  1 is publish every scan.  2 is
+   // publish at half rate, and so on.
 
   bool general_system_state;
   bool derived_settings;
